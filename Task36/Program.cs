fa92ee7af8b3cdba6f06269faaa0a01472bcc16f
Anastasia <kazantseva_nastya_95@mail.ru>
2022-11-04 20:41:44 +0500

    Task36

---
+++ Task36/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 36: Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+int[] GetArray(int size)
+{
+  int[] res = new int[size];
+  for (int i = 0; i < size; i++)
+  {
+    res[i] = new Random().Next(-100, 100);
+    Console.Write($"{res[i]}, ");
+  }
+  return res;
+}
+int[] array = GetArray(4);
+
+
+int sum = 0;
+
+for (int z = 1; z < array.Length; z+=2)
+    {
+      if (z % 2 == 1)
+        sum = sum + array[z];
+    }
+
+    Console.WriteLine($"Сумма элементов на нечётных позициях = {sum}");
---
+++ Task38/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 38: Задайте массив вещественных чисел. Найдите
+// разницу между максимальным и минимальным
+// элементов массива.
+// [3 7 22 2 78] -> 76
+
+int[] GetArray(int sije)
+{
+  int[] res = new int[sije];
+  for (int i = 0; i < sije; i++)
+  {
+    res[i] = new Random().Next(0, 100);
+    Console.Write($"{res[i]}, ");
+  }
+  return res;
+}
+int[] array = GetArray(5);
+
+int minValue = array[0];
+int maxValue = array[0];
+
+for (int j = 0; j < array.Length; j++)
+{
+ if (array[j] > maxValue)
+{
+  maxValue = array[j];
+  //Console.WriteLine($"макс{maxValue}, ");
+}
+{
+  if (array[j] < minValue)
+    minValue = array[j];
+    //Console.WriteLine($"мин{minValue}, "); 
+}
+}
+
+int result = maxValue - minValue;
+Console.WriteLine();
+Console.WriteLine($" разница {result}");
